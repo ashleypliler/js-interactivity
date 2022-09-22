e2d7d7c3b9b5d06339aee5594e622d5bd1af66e0
Ashley Pliler <ashleypliler1@gmail.com>
2022-09-21 19:04:48 -0600

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('hello world');
+
+const addMovie = (event) => {
+    let inputFeild = document.querySelector(event);
+    const movie = document.createElement('li');
+    const movieTitle = document.createElement('span');
+    movieTitle.textContent = inputFeild
+}
+
